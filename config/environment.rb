9dd77fb92bdb4e860e55191f37b4e77e172e214d
pzu <peter.zuercher@youngsolutions.ch>
2010-02-08 17:00:03 +0100

    no version for will_paginate

--- config/environment.rb
+++ config/environment.rb
@@ -16,7 +16,7 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on and have them installed with rake gems:install
   config.gem "RedCloth"
-  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
+  config.gem 'will_paginate' #, :version => '~> 2.3.11', :source => 'http://gemcutter.org'
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
